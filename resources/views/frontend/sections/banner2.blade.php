3e8dc8d96455e0be2a07ca5c45b8b260381a233a
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:18:09 +0530

    Updated many sections in frontend

---
+++ resources/views/frontend/sections/banner2.blade.php
@@ -0,0 +1,13 @@
+ <!-- banner section two starts -->
+ <section class="banner2 my-5">
+     <div class="container-fluid px-0">
+         <div class="row  justify-content-center align-items-center no-gutters">
+             <div class="col-md-12">
+                 <!-- <a href="https://api.whatsapp.com/send?phone=+91%2063669-87210" target="_blank" class="d-block"> -->
+                 <img src="{{ url('frontend/images/Static-Banner-W-1920px-H-580px-Feb.jpg') }}" alt=""
+                     class="img-fluid w-100">
+                 <!-- </a> -->
+             </div>
+         </div>
+     </div>
+ </section>
