bb154384186221eb1fd2d5ad84eb96c5b330261f
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:18:19 +0530

    Updated many sections in frontend

---
+++ resources/views/frontend/sections/promise.blade.php
@@ -0,0 +1,70 @@
+ <!-- promise section starts -->
+ <section class="promise mb-mb-5 mb-3 pt-2" id="our_promise">
+     <div class="container custom_container">
+         <div class="row mt-md-5 mt-3">
+
+             <h1 class="m-auto font-weight-bold headstore">We Promise You</h1>
+
+         </div>
+         <div class="row pt-5 text-center">
+             <div class="col-md-10 mx-auto">
+                 <div class="row">
+                     <div class="col-lg-4 col-6 border-rt border-bt">
+                         <div class="bggreen"><img src="{{ url('frontend/images/35-years.svg') }}" class="img-fluid">
+                         </div>
+                         <h6 class="font-weight-bold bluepromise pb-4 pt-3">Over 35 years’ legacy of care</h6>
+                     </div>
+                     <div class="col-lg-4 col-6 border-rt border-bt border-rtn">
+                         <div class="bggreen"><img
+                                 src="{{ url('frontend/images/Trained%20%26%20Caring%20Pharmacist.svg') }}"
+                                 class="img-fluid">
+                         </div>
+                         <h6 class="font-weight-bold bluepromise pb-4 pt-3">Trained & Caring Pharmacist</h6>
+                     </div>
+                     <div class="col-lg-4 col-6 border-bt border-rty">
+                         <div class="bggreen mtlg-0"><img src="{{ url('frontend/images/Genuine%20Medicines.svg') }}"
+                                 class="img-fluid">
+                         </div>
+                         <h6 class="font-weight-bold bluepromise pb-4 pt-3">Genuine Medicines</h6>
+                     </div>
+                     <div class="col-lg-4 col-6 border-rt d-lg-none d-block border-rtn border-bty">
+                         <div class="bggreen "><img
+                                 src="{{ url('frontend/images/Exhaustive%20Range%20of%20Products.svg') }}"
+                                 class="img-fluid">
+                         </div>
+                         <h6 class="font-weight-bold bluepromise pt-3">Exhaustive Range of Products</h6>
+                     </div>
+                 </div>
+             </div>
+         </div>
+
+         <div class="row text-center mb-5 pb-md-5 pb-3">
+             <div class="col-md-10 mx-auto">
+                 <div class="row">
+                     <div class="col-lg-4 col-6 border-rt d-lg-block d-none">
+                         <div class="bggreen mt-4"><img
+                                 src="{{ url('frontend/images/Exhaustive%20Range%20of%20Products.svg') }}"
+                                 class="img-fluid">
+                         </div>
+                         <h6 class="font-weight-bold bluepromise pt-3">Exhaustive Range of Products</h6>
+                     </div>
+                     <div class="col-lg-4 col-6 border-rt">
+                         <div class="bggreen mt-4"><img
+                                 src="{{ url('frontend/images/Medicine%20Refill%20Reminders.svg') }}" class="img-fluid">
+                         </div>
+                         <h6 class="font-weight-bold bluepromise pt-3">Medicine Refill Reminders</h6>
+                     </div>
+                     <div class="col-lg-4 col-6">
+                         <div class="bggreen mt-4"><img src="{{ url('frontend/images/Free%20Home%20Delivery.svg') }}"
+                                 class="img-fluid"></div>
+                         <h6 class="font-weight-bold bluepromise pt-3">Free Home Delivery</h6>
+                     </div>
+                 </div>
+             </div>
+
+         </div>
+
+
+     </div>
+ </section>
+ <!-- promise section ends -->
