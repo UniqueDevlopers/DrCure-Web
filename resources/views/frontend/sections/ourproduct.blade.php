6ae0201a1256e05a08b628e40928148e155fe4b4
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:18:16 +0530

    Updated many sections in frontend

---
+++ resources/views/frontend/sections/ourproduct.blade.php
@@ -0,0 +1,65 @@
+    <!-- our product section starts -->
+    <section class="ourproduct py-md-5 pt-3  mb-md-5 mb-0" id="product_range">
+        <div class="container custom_container">
+            <div class="row mt-md-5 mt-3">
+
+                <h1 class="m-auto font-weight-bold headstore">Our Product Range</h1>
+
+            </div>
+            <div class="row pt-5 text-center range">
+                <div class="col-lg-2 col-6 border-rt border-bt">
+                    <img src="{{ url('frontend/images/Prescription.svg') }}" class="img-fluid pb-3">
+                    <h6 class="font-weight-bold black pb-4   ">Prescription</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rt border-bt border-rtn">
+                    <img src="{{ url('frontend/images/OTC%20Drugs.svg') }}" class="img-fluid pb-3">
+                    <h6 class="font-weight-bold black pb-4"> OTC Drugs</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rt border-bt">
+                    <img src="{{ url('frontend/images/Personal%20care%20%26%20hygiene.svg') }}"
+                        class="img-fluid pb-3 mt-lg-0 mt-4">
+                    <h6 class="font-weight-bold black pb-4">Personal care & hygiene</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rt border-bt border-rtn">
+                    <img src="{{ url('frontend/images/Feminine%20care.svg') }}" class="img-fluid pb-3 mt-lg-0 mt-4">
+                    <h6 class="font-weight-bold black pb-4">Feminine care</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-bt border-rty">
+                    <img src="{{ url('frontend/images/Baby%20care.svg') }}" class="img-fluid pb-3 mt-lg-0 mt-4">
+                    <h6 class="font-weight-bold black pb-4">Baby care</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rt d-lg-none d-block border-rtn border-bty">
+                    <img src="{{ url('frontend/images/Wellness.svg') }}" class="img-fluid pb-3 pt-4">
+                    <h6 class="font-weight-bold black">Wellness</h6>
+                </div>
+            </div>
+
+            <div class="row text-center range mb-5">
+                <div class="col-lg-2 col-6 border-rt d-lg-block d-none">
+                    <img src="{{ url('frontend/images/Wellness.svg') }}" class="img-fluid pb-3 pt-4">
+                    <h6 class="font-weight-bold black">Wellness</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rt border-bty">
+                    <img src="{{ url('frontend/images/Sports%20nutrition.svg') }}" class="img-fluid pb-3 pt-4">
+                    <h6 class="font-weight-bold black">Sports nutrition</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rt border-rtn border-bty">
+                    <img src="{{ url('frontend/images/Surgical%20%26%20disposables.svg') }}"
+                        class="img-fluid pb-3 pt-4">
+                    <h6 class="font-weight-bold black">Surgical & disposables</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rt">
+                    <img src="{{ url('frontend/images/oximeter.svg') }}" class="img-fluid pb-3 pt-4">
+                    <h6 class="font-weight-bold black">Medical devices</h6>
+                </div>
+                <div class="col-lg-2 col-6 border-rtn">
+                    <img src="{{ url('frontend/images/And%20much%2c%20much%20more.svg') }}"
+                        class="img-fluid pb-3 pt-4">
+                    <h6 class="font-weight-bold black">And much, much more</h6>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    <!-- our product section ends -->
