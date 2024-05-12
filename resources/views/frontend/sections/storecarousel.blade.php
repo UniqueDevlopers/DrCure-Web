704e6e47c40fae7f9bd1a85e05c3497a9ccfa71e
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:18:22 +0530

    Updated many sections in frontend

---
+++ resources/views/frontend/sections/storecarousel.blade.php
@@ -0,0 +1,136 @@
+  <!-- store carousel section starts -->
+  <section class="storecarousel">
+      <div class="container custom_container">
+          <div class="row justify-content-center">
+              <div class="col-lg-12">
+                  <div id="owl-demo-container">
+                      <div id="owl-demo" class="owl-carousel owl-theme mt-0 bannerimage_slide">
+                      </div>
+                  </div>
+              </div>
+
+
+              <div class="owl-carousel owl-theme mt-0 bannerimage_slide d-none" id="storecarousel_slide">
+                  <div class="item pt-5 px-3">
+                      <div class="bg-blue1 pl-3 pt-3">
+                          <h3 class="text-white font-weight-bold pb-3">Electronic city phase 1</h1>
+                              <div class="row">
+                                  <div class="col-2"> <img src="{{ url('frontend/images/location pin icon.svg') }}"
+                                          class="img-fluid">
+                                  </div>
+                                  <div class="col-10">
+                                      <p class="text-white">Shop no.04, Prime Building, Near Play ground, Banglore,
+                                          Karnataka 04</p>
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="col-2"> <img src="{{ url('frontend/images/callicon.svg') }}"
+                                          class="img-fluid"></div>
+                                  <div class="col-10">
+                                      <p class="text-white">+91 99999 00000</p>
+                                  </div>
+                              </div><br>
+                              <div class="row">
+                                  <div class="col-7">
+
+                                      <a href="#" onclick="viewmap(25.09192,55.24369)"
+                                          class="blue h5 font-weight-bold bimg">Show Google Map</a>
+                                  </div>
+                                  <div class="col-5"> <img
+                                          src="{{ url('frontend/images/shop icon card background.svg') }}"
+                                          class="img-fluid mrd"></div>
+                              </div>
+                      </div>
+                  </div>
+
+                  <div class="item pt-5 px-3">
+                      <div class="bg-blue2 pl-3 pt-3">
+                          <h3 class="text-white font-weight-bold pb-3">Electronic city phase 1</h1>
+                              <div class="row">
+                                  <div class="col-2"> <img src="{{ url('frontend/images/locationwhite.svg') }}"
+                                          class="img-fluid"> </div>
+                                  <div class="col-10">
+                                      <p class="text-white">Shop no.04, Prime Building, Near Play ground, Banglore,
+                                          Karnataka 04</p>
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="col-2"> <img
+                                          src="{{ url('frontend/images/call icon free delivery.svg') }}"
+                                          class="img-fluid">
+                                  </div>
+                                  <div class="col-10">
+                                      <p class="text-white">+91 99999 00000</p>
+                                  </div>
+                              </div><br>
+                              <div class="row">
+                                  <div class="col-7"> <a href="#" onclick="viewmap(25.40481,55.43023)"
+                                          class="text-white h5 font-weight-bold bimg">Show Google Map</a></div>
+                                  <div class="col-5"> <img src="{{ url('frontend/images/shopicon2.svg') }}"
+                                          class="img-fluid mrd"></div>
+                              </div>
+                      </div>
+                  </div>
+
+                  <div class="item pt-5 px-3">
+                      <div class="bg-blue1 pl-3 pt-3">
+                          <h3 class="text-white font-weight-bold pb-3">Dasarahalli</h1>
+                              <div class="row">
+                                  <div class="col-2"> <img src="images/location pin icon.svg" class="img-fluid">
+                                  </div>
+                                  <div class="col-10">
+                                      <p class="text-white">Shop no.04, Prime Building, Near Play ground, Banglore,
+                                          Karnataka 04</p>
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="col-2"> <img src="{{ url('frontend/images/callicon.svg') }}"
+                                          class="img-fluid"></div>
+                                  <div class="col-10">
+                                      <p class="text-white">+91 99999 00000</p>
+                                  </div>
+                              </div><br>
+                              <div class="row">
+                                  <div class="col-7"> <a href="#" onclick="viewmap(25.27646,55.33674)"
+                                          class="blue h5 font-weight-bold bimg">Show Google Map</a></div>
+                                  <div class="col-5"> <img
+                                          src="{{ url('frontend/images/shop icon card background.svg') }}"
+                                          class="img-fluid mrd"></div>
+                              </div>
+                      </div>
+                  </div>
+
+                  <div class="item pt-5 px-3">
+                      <div class="bg-blue2 pl-3 pt-3">
+                          <h3 class="text-white font-weight-bold pb-3">Electronic city phase 1</h1>
+                              <div class="row">
+                                  <div class="col-2"> <img src="{{ url('frontend/images/locationwhite.svg') }}"
+                                          class="img-fluid"> </div>
+                                  <div class="col-10">
+                                      <p class="text-white">Shop no.04, Prime Building, Near Play ground, Banglore,
+                                          Karnataka 04</p>
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="col-2"> <img
+                                          src="{{ url('frontend/images/call icon free delivery.svg') }}"
+                                          class="img-fluid">
+                                  </div>
+                                  <div class="col-10">
+                                      <p class="text-white">+91 99999 00000</p>
+                                  </div>
+                              </div><br>
+                              <div class="row">
+                                  <div class="col-7"> <a href="#" onclick="viewmap(25.28057,55.35163)"
+                                          class="text-white h5 font-weight-bold bimg">Show Google Map</a></div>
+                                  <div class="col-5"> <img src="{{ url('frontend/images/shopicon2.svg') }}" t
+                                          class="img-fluid mrd"></div>
+                              </div>
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- store carousel section ends -->
