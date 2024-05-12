a9a3715d3e4626224411d3ea9a32066102c9ee42
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:18:00 +0530

    Updated many sections in frontend

---
+++ resources/views/frontend/sections/aboutus.blade.php
@@ -0,0 +1,36 @@
+ <!-- friendly section starts -->
+ <section class="friendly" id="about_us">
+     <div class="container custom_container">
+         <div class="row mt-md-5 pt-md-5 pt-3 mt-3 align-items-center">
+             <div class="col-lg-6 d-lg-none d-block mbres">
+                 <img src="{{ url('frontend/images/neighbourhood_pharmacy.jpg') }}" class="img-fluid">
+             </div>
+
+             <div class="col-lg-5">
+                 <h1 class="m-auto font-weight-bold headstore d-lg-block d-none">Your Friendly Neighbourhood Pharmacy
+                 </h1>
+                 <div class="bgfoot bgeffect p-5 mt-md-4 mb-md-5">
+                     <h1 class="m-auto font-weight-bold text-center text-white pb-3 d-lg-none d-block">Your Friendly
+                         <br>Neighbourhood Pharmacy
+                     </h1>
+                     <p class="text-white txtcenter-mob"><strong>Aster Pharmacy</strong> is one of the largest retail
+                         pharmacy groups in the middle east and is now present in India. We opened India’s 1st
+                         friendly neighbourhood pharmacy in February 2021 and are rapidly expanding.</p>
+                     <p class="text-white txtcenter-mob"> With 3 decades of experience in the health care industry,
+                         we are proud to bring a friendly and experienced approach to your neighbourhood and are
+                         working to be the pharmacy that cares for your community. We care for you and promise you
+                         with friendly and trained pharmacists, pleasant ambience, genuine medicines, exhaustive
+                         range of products, medicine refill reminders and free home delivery.</p>
+                     <h4 class="bluehead font-weight-bold txtcenter-mob">Your wellness is our priority.</h4>
+                     <h4 class="bluehead font-weight-bold txtcenter-mob">We welcome you to experience your Friendly
+                         Neighbourhood Pharmacy.</h4>
+                 </div>
+             </div>
+             <div class="col-lg-7 d-lg-block d-none">
+                 <img src="{{ url('frontend/images/neighbourhood_pharmacy.jpg') }}" class="img-fluid ht">
+             </div>
+
+         </div>
+     </div>
+ </section>
+ <!-- friendly section ends -->
