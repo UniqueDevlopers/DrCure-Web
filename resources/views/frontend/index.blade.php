d5dc24758ea56e7a5f557642fdc4d585cb6718d1
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:17:54 +0530

    Updated many sections in frontend

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.frontend.app')
 @section('content')
-
-
+    @include('frontend.sections.banner')
+    @include('frontend.sections.storefinder')
+    @include('frontend.sections.storecarousel')
+    @include('frontend.sections.banner2')
+    @include('frontend.sections.aboutus')
+    @include('frontend.sections.ourproduct')
+    @include('frontend.sections.promise')
+    @include('frontend.sections.talent')
+    @include('frontend.sections.feedback')
+    @include('frontend.sections.associate')
+    @include('frontend.sections.newfeedback')
 @endsection
