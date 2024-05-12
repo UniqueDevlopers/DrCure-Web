6eb2416ae4ec5f6cdafa81b4a12824e243a18071
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:18:27 +0530

    Updated many sections in frontend

---
+++ resources/views/frontend/sections/talent.blade.php
@@ -0,0 +1,35 @@
+ <!-- talents section starts -->
+ <section class="talents" id="careers">
+     <div class="container">
+         <div class="row mt-5 mb-5 pt-3 align-items-center">
+
+
+             <div class="col-lg-6">
+                 <div class="py-5 pr-5 career">
+                     <h1 class="m-auto font-weight-bold headstore txtcenter-mob">Careers</h1>
+                     <p class="black pt-3 pb-3 txtcenter-mob">Join Aster Pharmacy – India & give a boost to your
+                         career. Join us to redefine the retail pharmacy sector.</p>
+                     <!-- <p class="bluepromise bor-bot font-weight-bold">Pharmacist – Bangalore (10)</p>
+                        <p class="bluepromise bor-bot font-weight-bold">Pharmacist – Kochi (5)</p> -->
+                     <p class="bluepromise bor-bot font-weight-bold">Pharmacist (3)</p>
+                     <p class="bluepromise bor-bot font-weight-bold">Executive Operations (3)</p>
+                     <p class="bluepromise bor-bot font-weight-bold">Executive Sales (3)</p>
+                     <p class="bluepromise bor-bot font-weight-bold">Executive accounts (3)</p>
+                     <p class="bluepromise bor-bot font-weight-bold">Merchandiser (3)</p>
+                     <p class="bluepromise bor-bot font-weight-bold">Business Analyst (1)</p>
+                     <p class="bluepromise bor-bot font-weight-bold">Manager - Human Resources (1)</p>
+                     <p class="bluepromise bor-bot font-weight-bold">Assistant Pharmacist (3)</p>
+
+                     <button class="btn text-white btnblue font-weight-bold mt-3 txtcenter-mobbtn">
+                         <a href="https://hcdt.fa.us2.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX"
+                             target="_blank" class="text-white"> Apply Now</a></button>
+                 </div>
+             </div>
+             <div class="col-lg-6 d-lg-block d-none">
+                 <img src="{{ url('frontend/images/career.jpg') }}" class="img-fluid">
+             </div>
+
+         </div>
+     </div>
+ </section>
+ <!-- talents section ends -->
