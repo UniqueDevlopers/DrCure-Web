829869284f1dbf0980e2c679bde9deb9a667ff69
nkhandwe <nkhandwe@gmail.com>
2024-05-12 08:18:03 +0530

    Updated many sections in frontend

---
+++ resources/views/frontend/sections/associate.blade.php
@@ -0,0 +1,47 @@
+ <!-- associate section starts -->
+ <section class="associate_divv mb-5 py-lg-5 d-lg-none d-block" id="associate">
+     <div class="container">
+         <div class="row justify-content-center">
+
+             <h1 class="m-auto text-center font-weight-bold headstore py-3">Associate with Aster Pharmacy as a Vendor
+                 / Business Leads</h1>
+
+
+             <form class="associate-form shadow rounded p-3 mt-5 mb-5" name="associate-form" id="associate_form"
+                 action="https://asterpharmacy.in/?" method="POST">
+                 <div id="allerror_asso" class="px-4 mb-1 text-black font-weight-bold pt-3 h5"></div>
+                 <input type="hidden" id="utm_source" name="utm_source" value="" />
+                 <input type="hidden" id="utm_medium" name="utm_medium" value="" />
+                 <input type="hidden" id="utm_campaign" name="utm_campaign" value="" />
+                 <input type="hidden" id="create_date" name="create_date" value="" />
+                 <div class="row px-4 pb-4 pt-0">
+
+                     <div class="input-group mb-3 mt-3">
+                         <input type="text" class="form-control" placeholder="Name" name="name" id="name_asso">
+                     </div>
+
+                     <div class="input-group mb-3">
+                         <input type="text" class="form-control" placeholder="Email ID" id="email_asso"
+                             name="email">
+                     </div>
+
+                     <div class="input-group mb-3">
+                         <input type="text" class="form-control" placeholder="Phone Number" id="mobile_asso"
+                             name="mobile">
+                     </div>
+                     <div id="g-recaptcha2" class="input-group mb-3 py-2"></div>
+                     <input type="hidden" id="g-res2" name="g-res2" />
+
+
+                     <input class="btn btnblue mt-3 text-white m-auto" id="submit_btn_asso" type="button"
+                         value="Submit">
+                     <!-- <button class="btn btnblue mt-3 text-white m-auto" id="submit_btn_asso" type="button"> Submit
+                        </button> -->
+
+                 </div>
+
+             </form>
+         </div>
+     </div>
+
+ </section>
