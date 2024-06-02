185ef420b6c028841cf6c3ab03e16b28202a091b
Narendra Khandwe <nkhandwe@gmail.com>
2024-06-02 13:10:49 +0530

    Updated Chnages on files for backend

---
+++ app/Http/Controllers/ManageContent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageContent extends Controller
+{
+    public function index(){
+
+        return view();
+    }
+}
