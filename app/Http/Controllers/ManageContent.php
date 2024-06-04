8be74496346d6fe29f870ce9186c7ad3d776871d
Narendra Khandwe <nkhandwe@gmail.com>
2024-06-04 22:36:02 +0530

    updated to the admin part

--- app/Http/Controllers/ManageContent.php
+++ app/Http/Controllers/ManageContent.php
@@ -2,18 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Slider;
+use App\Models\StoreBranch;
 use Illuminate\Http\Request;
 
 class ManageContent extends Controller
 {
     public function sliderManage()
     {
+        $slider = Slider::all();
+        return view('admin.slider-manage.index', compact('slider'));
+    }
 
-        return view('admin.slider-manage.index');
+    public function sliderPost(Request $request)
+    {
+        $slider = new Slider();
+        $slider->button_link = $request->button_link;
+        if ($request->hasFile('image')) {
+            $image = time() . '_slider' . '.' . $request->image->extension();
+            $request->image->move(public_path('uploads/slider'), $image);
+            $slider->image = 'uploads/slider/' . $image;
+        }
+        $slider->save();
+        if ($slider) {
+            return back();
+        } else {
+            return back();
+        }
     }
     public function storeManage()
     {
+        $branch = StoreBranch::all();
+        return view('admin.store-manage.index', compact('branch'));
+    }
 
-        return view('admin.store-manage.index');
+    public function storePost(Request $request)
+    {
+        $branch = new StoreBranch();
+        $branch->store_name = $request->store_name;
+        $branch->address = $request->address;
+        $branch->phone_number = $request->phone_number;
+        $branch->longitude = $request->longitude;
+        $branch->latitude = $request->latitude;
+        $branch->save();
+        if ($branch) {
+            return redirect()->back();
+        } else {
+            return redirect()->back();
+        }
     }
 }
