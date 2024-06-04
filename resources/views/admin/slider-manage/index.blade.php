56db631ab50d1703383749fdf53b7ad9069ecdd2
Narendra Khandwe <nkhandwe@gmail.com>
2024-06-04 22:36:18 +0530

    updated to the admin part

--- resources/views/admin/slider-manage/index.blade.php
+++ resources/views/admin/slider-manage/index.blade.php
@@ -22,20 +22,21 @@
                         <h1 class="fs-3">Upload Banner Images Here.</h1>
 
                         <hr class="mb-3 mt-3 h-2 text-danger">
-                        <form action="" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('slider-post') }}" method="post" enctype="multipart/form-data">
+                            @csrf
                             <div class="col-4">
                                 <label class="form-label mt-2" for="link">Upload Image</label>
                                 <div class="form-group mb-2">
                                     <div class="add-file btn btn-danger position-relative overflow-hidden">
                                         <i class="las la-cloud-upload-alt me-2 font-15"></i>Select Image
-                                        <input type="file" name="file" class="add-file-input" />
+                                        <input type="file" name="image" class="add-file-input" />
                                     </div>
                                 </div>
                             </div>
                             <div class="col-4">
                                 <div class="form-group mb-2">
                                     <label class="form-label" for="link">Button Link</label>
-                                    <input type="text" class="form-control" id="link" name="link"
+                                    <input type="text" class="form-control" id="link" name="button_link"
                                         placeholder="Enter Button Link">
                                 </div>
                             </div>
@@ -44,9 +45,40 @@
                             </div>
                         </form>
                     </div><!--end col-->
+                    <h1 class="fs-1 mt-4 mb-3">List Sliders</h1>
+                    <hr>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">SL</th>
+                                <th scope="col">Image</th>
+                                <th scope="col">Link</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($slider as $key => $value)
+                                <tr>
+                                    <th scope="row">{{ $key + 1 }}</th>
+                                    <td><img src="{{ URL::to($value->image) }}" height="100px" width="100px"
+                                            alt=""></td>
+                                    <td>{{ $value->button_link }}</td>
+                                    <td>
+                                        @if ($value->isActive == 1)
+                                            <button class="btn btn-success brn-sm">Active</button>
+                                        @else
+                                            <button class="btn btn-danger brn-sm">InActive</button>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-danger"> <i class="fa fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-                <!-- end page title end breadcrumb -->
-
             </div><!-- container -->
         </div><!-- container -->
     @endsection
