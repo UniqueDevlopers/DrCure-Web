7d342638645cd782af7ccab3e5d95ee14b28fc10
Narendra Khandwe <nkhandwe@gmail.com>
2024-06-04 22:36:09 +0530

    updated to the admin part

--- database/migrations/2024_06_02_081101_create_sliders_table.php
+++ database/migrations/2024_06_02_081101_create_sliders_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->nullable();
+            $table->string('button_link')->nullable();
+            $table->boolean('isActive')->default(1);
             $table->timestamps();
         });
     }
