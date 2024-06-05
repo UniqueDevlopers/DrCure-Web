03d3148b5b69b2e29b221dc30b67a5e2116d9017
Narendra Khandwe <nkhandwe@gmail.com>
2024-06-05 11:27:05 +0530

    "Added two new routes for hero banner management"

--- resources/views/admin/hero-manage/index.blade.php
+++ resources/views/admin/hero-manage/index.blade.php
@@ -13,7 +13,7 @@
                                 <li class="breadcrumb-item"><a href="#">DrCure</a>
                                 </li>
                                 <!--end nav-item-->
-                                <li class="breadcrumb-item active"><a href="#">Store Manage</a>
+                                <li class="breadcrumb-item active"><a href="#">Hero Section Manage</a>
                                 </li>
                                 <!--end nav-item-->
                             </ol>
@@ -25,42 +25,45 @@
                         <h1 class="fs-3">Manage Stores/Branches Here.</h1>
 
                         <hr class="mb-3 mt-3 h-2 text-danger">
-                        <form action="{{ route('store-post') }}" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('hero-post') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <div class="row">
                                 <div class="col-4">
                                     <div class="form-group mb-2">
-                                        <label class="form-label" for="store_name">Store Name</label>
-                                        <input type="text" class="form-control" id="store_name" name="store_name"
-                                            placeholder="Enter Store Name">
+                                        <label class="form-label" for="heading_top">Heading Top</label>
+                                        <input type="text" class="form-control" id="heading_top" name="heading_top"
+                                            placeholder="Enter Heading Top" value="">
                                     </div>
                                 </div>
+
                                 <div class="col-4">
                                     <div class="form-group mb-2">
-                                        <label class="form-label" for="address">Address</label>
-                                        <input type="text" class="form-control" id="address" name="address"
-                                            placeholder="Enter Address">
+                                        <label class="form-label" for="heading_bottom">Heading Bottom</label>
+                                        <input type="text" class="form-control" id="heading_bottom" name="heading_bottom"
+                                            placeholder="Enter Heading Bottom">
                                     </div>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-2">
                                     <div class="form-group mb-2">
-                                        <label class="form-label" for="phone_number">Phone Number</label>
-                                        <input type="text" class="form-control" id="phone_number" name="phone_number"
-                                            placeholder="Enter Phone Number">
+                                        <label class="form-label" for="bg_color">Background Color</label>
+                                        <input type="color" class="form-control" id="bg_color" name="bg_color"
+                                            placeholder="Enter Background Color">
                                     </div>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-2">
+                                    <label class="form-label mt-2" for="link">Upload Image</label>
                                     <div class="form-group mb-2">
-                                        <label class="form-label" for="longitude">Longitude</label>
-                                        <input type="text" class="form-control" id="longitude" name="longitude"
-                                            placeholder="Enter Longitude">
+                                        <div class="add-file btn btn-danger position-relative overflow-hidden">
+                                            <i class="las la-cloud-upload-alt me-2 font-15"></i>Select Image
+                                            <input type="file" name="image" class="add-file-input" />
+                                        </div>
                                     </div>
+
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12">
                                     <div class="form-group mb-2">
-                                        <label class="form-label" for="latitude">Latitude</label>
-                                        <input type="text" class="form-control" id="latitude" name="latitude"
-                                            placeholder="Enter Latitude">
+                                        <label class="form-label" for="description">Description</label>
+                                        <textarea type="text" class="form-control" id="description" name="description" placeholder="Enter description"></textarea>
                                     </div>
                                 </div>
                                 <div class="col-4 mt-4">
@@ -73,43 +76,6 @@
                     <!--end col-->
                 </div>
                 <!-- end page title end breadcrumb -->
-                <h1 class="fs-1 mt-4 mb-3">List Sliders</h1>
-                <hr>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">SL</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Address</th>
-                            <th scope="col">Phone Number</th>
-                            <th scope="col">Long/Lati</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($branch as $key => $value)
-                            <tr>
-                                <th scope="row">{{ $key + 1 }}</th>
-                                <td>{{ $value->store_name }}
-                                </td>
-                                <td>{{ $value->address }}</td>
-                                <td>{{ $value->phone_number }}</td>
-                                <td>{{ $value->longitude }}/{{ $value->latitude }}</td>
-                                <td>
-                                    @if ($value->isActive == 1)
-                                        <button class="btn btn-success btn-sm">Active</button>
-                                    @else
-                                        <button class="btn btn-danger btn-sm">InActive</button>
-                                    @endif
-                                </td>
-                                <td>
-                                    <button class="btn btn-danger btn-sm"> <i class="fa fa-trash"></i></button>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
             </div>
             <!-- container -->
         </div>
