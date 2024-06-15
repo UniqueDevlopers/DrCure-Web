389f191c79809aca3a541f7fbdcd7497ccb227b0
nkhandwe <nkhandwe@gmail.com>
2024-06-15 23:55:02 +0530

    updated

--- resources/views/frontend/sections/aboutus.blade.php
+++ resources/views/frontend/sections/aboutus.blade.php
@@ -1,4 +1,7 @@
  <!-- friendly section starts -->
+ @php
+     $hero = \App\Models\HeroSection::first();
+ @endphp
  <section class="friendly" id="about_us">
      <div class="container custom_container">
          <div class="row mt-md-5 pt-md-5 pt-3 mt-3 align-items-center">
@@ -7,27 +10,48 @@
              </div>
 
              <div class="col-lg-5">
-                 <h1 class="m-auto font-weight-bold headstore d-lg-block d-none">Your Friendly Neighbourhood Pharmacy
+                 <h1 class="m-auto font-weight-bold headstore d-lg-block d-none">{{ $hero->heding_top }}
                  </h1>
                  <div class="bgfoot bgeffect p-5 mt-md-4 mb-md-5">
-                     <h1 class="m-auto font-weight-bold text-center text-white pb-3 d-lg-none d-block">Your Friendly
-                         <br>Neighbourhood Pharmacy
-                     </h1>
-                     <p class="text-white txtcenter-mob"><strong>DrCure Pharmacy</strong> is one of the largest retail
-                         pharmacy groups in the middle east and is now present in India. We opened India’s 1st
-                         friendly neighbourhood pharmacy in February 2021 and are rapidly expanding.</p>
-                     <p class="text-white txtcenter-mob"> With 3 decades of experience in the health care industry,
-                         we are proud to bring a friendly and experienced approach to your neighbourhood and are
-                         working to be the pharmacy that cares for your community. We care for you and promise you
-                         with friendly and trained pharmacists, pleasant ambience, genuine medicines, exhaustive
-                         range of products, medicine refill reminders and free home delivery.</p>
-                     <h4 class="bluehead font-weight-bold txtcenter-mob">Your wellness is our priority.</h4>
-                     <h4 class="bluehead font-weight-bold txtcenter-mob">We welcome you to experience your Friendly
-                         Neighbourhood Pharmacy.</h4>
+                     {{-- <h1 class="m-auto font-weight-bold text-center text-white pb-3 d-lg-none d-block">
+                         {{ $hero->heading_bottom }}
+                     </h1> --}}
+                     <p class="text-white txtcenter-mob"><strong>Dr.Cure Pharmacy</strong>, a prominent retail pharmacy
+                         group in the
+                         telangana andhra, has expanded its presence to India. In February 2018, we proudly opened
+                         India's first friendly neighborhood pharmacy, and since then, we have been rapidly growing. Our
+                         stores are strategically located in well-established residential communities and prime retail
+                         locations across major cities in Telangana.
+                         <br>
+                         <br>
+                         As we continue to expand, our primary focus remains on providing innovative healthcare services
+                         to our valued customers. At Dr Cure Pharmacy, we are dedicated to delivering high-quality,
+                         professional, and convenient pharmacy services. In addition to prescription drugs and
+                         over-the-counter medications, we offer a wide range of merchandise, including nutritional
+                         supplements, herbal products, personal care items, family care products, and various
+                         convenience products such as consumables, seasonal items, and promotional goods.
+                         <br>
+                         <br>
+                         With a decade of experience in the healthcare industry, we take pride in bringing a friendly
+                         and experienced approach to your neighbourhood. Our goal is to be the pharmacy that genuinely
+                         cares for your community. We prioritize your well-being by ensuring friendly and trained
+                         pharmacists, a pleasant ambience, genuine medicines, an extensive range of products, medicine
+                         refill reminders, and free home delivery.
+                         <br>
+                         <br>
+                         Our commitment to our customers is unwavering, and we continuously strive to enhance our
+                         services. We firmly believe that superior customer service is crucial for the success of retail
+                         companies. Therefore, we are dedicated to providing exceptional service in all our stores,
+                         every day, for each and every customer.
+                     </p>
+                     <h5 class="bluehead font-weight-bold txtcenter-mob" class="font-size:20px!important">
+                         {{ $hero->heading_bottom }}</h5>
+                     {{-- <h4 class="bluehead font-weight-bold txtcenter-mob">We welcome you to experience your Friendly
+                         Neighbourhood Pharmacy.</h4> --}}
                  </div>
              </div>
              <div class="col-lg-7 d-lg-block d-none">
-                 <img src="{{ url('frontend/images/neighbourhood_pharmacy.jpg') }}" class="img-fluid ht">
+                 <img src="{{ URL::to($hero->image) }}" class="img-fluid ht">
              </div>
 
          </div>
